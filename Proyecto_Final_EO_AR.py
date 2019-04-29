c4b8ab6b87c9af9f0065752bd076821fe9771053
eduor96 <35460766+eduor96@users.noreply.github.com>
2019-04-29 15:52:30 -0500

    Importación de Librerías
    Se agregan librerías por utilizar, incluyendo paquetería para optimizar

---
+++ Proyecto_Final_EO_AR.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 29 11:35:53 2019
+
+@author: eduor
+"""
+
+import numpy as np
+from pyswarm import pso
+import pandas as pd
+from oandapyV20 import API
+import oandapyV20.endpoints.instruments as instruments
+from dateutil.relativedelta import relativedelta
+import numpy as np
+import math
+import time
