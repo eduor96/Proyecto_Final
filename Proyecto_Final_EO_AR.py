99b6844aa42b4a00ab2d75690de1e8e3137824f2
Angel Ruiz <angelrenerg@hotmail.com>
2019-05-02 09:56:59 -0500

    Funciones a utilizar

--- Proyecto_Final_EO_AR.py
+++ Proyecto_Final_EO_AR.py
@@ -14,3 +14,39 @@ from dateutil.relativedelta import relativedelta
 import numpy as np
 import math
 import time
+#%%
+# =============================================================================
+# Definimos funciones a utilizar
+# =============================================================================
+
+def date_range(start_date, end_date, increment, period):
+    #Funcion que crea vector de fechas con incremento especifico
+    result = []
+    nxt = start_date
+    delta = relativedelta(**{period:increment})
+    while nxt <= end_date:
+        result.append(nxt)
+        nxt += delta
+    return result
+
+def ag_car(cadena):
+    #Funcion que agrega caracteres a cadena, fin especifico para funcion de oanda
+    nueva=cadena[0:10]+"T"+cadena[11:-1]+"Z"
+    return nueva
+def rsi_fun(prices,ind,n):
+    #Funcion que obtiene el RSI de ciertos precios con cierto periodo
+    
+    gains=[]
+    losses=[]
+    ind=ind-(n)
+    for i in range(n) :
+        dife=float(prices[ind+1])-float(prices[ind])
+        if dife>=0:
+            gains.append(dife)
+        else:
+            losses.append(dife*-1)
+        ind=ind+1
+    gains=np.array(gains)
+    losses=np.array(losses)
+    rsi=100-(100/(1+((gains.sum()/14)/(losses.sum()/14))))
+    return rsi
